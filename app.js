b47f13add8aa2953ff3b651aae8e8b308526bfda
chuck <chuckinthestar@gmail.com>
2017-04-14 13:53:39 +0900

    complete scripts

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const {
+  TWIBOT_TWITTER_KEY, TWIBOT_TWITTER_SECRET,
+  TWIBOT_TWITTER_TOKEN, TWIBOT_TWITTER_TOKEN_SECRET,
+  PORT
+} = process.env
+
+const options = {
+  consumer_key: TWIBOT_TWITTER_KEY,
+  consumer_secret: TWIBOT_TWITTER_SECRET,
+  access_token_key: TWIBOT_TWITTER_TOKEN,
+  access_token_secret: TWIBOT_TWITTER_TOKEN_SECRET,
+}
+app.set('options', options)
+
+app.set('port', (PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  res.send('This is Twitter-bot application.')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`Node app is running at localhost: ${app.get('port')}`)
+})
+
+module.exports = app
---
+++ bin/index.js
@@ -0,0 +1,34 @@
+const app = require('../app')
+const Twitter = require('twitter')
+const CronJob = require('cron').CronJob
+
+const {
+  consumer_key, consumer_secret, access_token_key, access_token_secret
+} = app.get('options')
+
+const client = new Twitter({
+  consumer_key,
+  consumer_secret,
+  access_token_key,
+  access_token_secret,
+})
+
+const cromTime = '0 0 0-14 * * *'
+
+const tweet = () => {
+  const message = 'test'
+  console.log(message)
+  client.post('statuses/update', { status: message }, (error, tweet, response) => {
+    if(error) {
+      console.error('error', error)
+    }
+  })
+}
+
+new CronJob({
+  cronTime,
+  onTick: () => {
+    tweet()
+  },
+  start: true,
+})
