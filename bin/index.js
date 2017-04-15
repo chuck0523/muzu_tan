9b921e4ab4f3f5a1c2c146d91816871ee141a502
chuck <chuckinthestar@gmail.com>
2017-04-15 13:27:48 +0900

    cron task to unfollow user who is not relate to me anymore

--- bin/index.js
+++ bin/index.js
@@ -14,7 +14,7 @@ const client = new Twitter({
 })
 
 // tweet
-const cronTime = '0 0 0-14 * * *'
+const tweetTime = '0 0 0-14 * * *'
 
 const tweet = () => {
   const message = 'test'
@@ -23,7 +23,7 @@ const tweet = () => {
     .catch(error => console.error(`Failed to tweet ${tweet}.`))
 }
 new CronJob({
-  cronTime,
+  cronTime: tweetTime,
   onTick: () => {
     tweet()
   },
@@ -33,9 +33,32 @@ new CronJob({
 // follow back
 const userStream = client.stream('user')
 userStream.on('follow', (data) => {
-  console.log(`Follow detected from userId: ${data.source.id_str}`)
-  const param = { user_id: data.source.id_str }
   client.post('friendships/create', { user_id: data.source.id_str })
-    .then(res => console.log(`Successfully followed back: ${res}`))
+    .then(res => console.log(`Successfully followed back: ${res.name}`))
     .catch(error => console.error(`Failed to folllow back: ${error}`))
+})
+
+
+// unfollow
+const unfollowTime = '0 0 0-14 * * *'
+
+const unfollow = () => {
+  client.get('friends/list', {})
+    .then(({ users }) => {
+      const user = users.find(user => !user.live_following)
+      if(user === undefined) {
+        console.log('There\'s no users to unfollow')
+        return
+      }
+      client.post('friendships/destroy', { user_id: user.id_str })
+      console.log(`Successfully unfollow: ${user.name}`)
+    })
+    .catch(error => console.error(`Failed to unfolllow: ${error}`))
+}
+new CronJob({
+  cronTime: unfollowTime,
+  onTick: () => {
+    unfollow()
+  },
+  start: true,
 })
