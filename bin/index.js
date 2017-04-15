9d299e6da9506d115247c38785f1ca7488c10694
chuck <chuckinthestar@gmail.com>
2017-04-15 12:48:02 +0900

    follow back feature

--- bin/index.js
+++ bin/index.js
@@ -13,6 +13,7 @@ const client = new Twitter({
   access_token_secret,
 })
 
+// tweet
 const cronTime = '0 0 0-14 * * *'
 
 const tweet = () => {
@@ -21,11 +22,20 @@ const tweet = () => {
     .then(tweet => console.log(`${tweet} was successfully tweeted.`))
     .catch(error => console.error(`Failed to tweet ${tweet}.`))
 }
-
 new CronJob({
   cronTime,
   onTick: () => {
     tweet()
   },
   start: true,
+})
+
+// follow back
+const userStream = client.stream('user')
+userStream.on('follow', (data) => {
+  console.log(`Follow detected from userId: ${data.source.id_str}`)
+  const param = { user_id: data.source.id_str }
+  client.post('friendships/create', { user_id: data.source.id_str })
+    .then(res => console.log(`Successfully followed back: ${res}`))
+    .catch(error => console.error(`Failed to folllow back: ${error}`))
 })
