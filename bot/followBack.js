39f7af6bac3a3cd13d2f2f554329f701a2c9d579
chuck <chuckinthestar@gmail.com>
2017-04-18 01:11:39 +0900

    add logginh

--- bot/followBack.js
+++ bot/followBack.js
@@ -1,5 +1,6 @@
 module.exports = (twitter) => {
   twitter.userStream.on('follow', (data) => {
+    console.log(`Detected follow by ${data.source.name}`)
     twitter.follow(data.source.id_str)
       .then(res => console.log(`Successfully followed back: ${res.name}`))
       .catch(error => console.error(`Failed to folllow back: ${error}`))
