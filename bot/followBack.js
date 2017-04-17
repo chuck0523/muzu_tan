3b31fdc17ff349bc8c314c70a794761e4dcef61f
chuck <chuckinthestar@gmail.com>
2017-04-18 00:26:36 +0900

    exec lib function at precise file

--- bot/followBack.js
+++ bot/followBack.js
@@ -1,7 +1,5 @@
 module.exports = (twitter) => {
-  const userStream = twitter.stream('user')
-
-  userStream.on('follow', (data) => {
+  twitter.userStream.on('follow', (data) => {
     twitter.follow(data.source.id_str)
       .then(res => console.log(`Successfully followed back: ${res.name}`))
       .catch(error => console.error(`Failed to folllow back: ${error}`))
