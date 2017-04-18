6b729d179acd6c776553d61977cab6ed413e84ed
chuck <chuckinthestar@gmail.com>
2017-04-18 22:36:42 +0900

    cancel own follow event

--- bot/followBack.js
+++ bot/followBack.js
@@ -1,6 +1,11 @@
 module.exports = (twitter) => {
   twitter.userStream.on('follow', (data) => {
-    // TODO: If myself return
+
+    // @muzu_tan's follow event
+    if(data.source.id_str === '771649236') { // TODO: Dedifine as constant
+      return
+    }
+
     twitter.follow(data.source.id_str)
       .then(res => console.log(`Successfully followed back: ${res.name}`))
       .catch(error => console.error(`Failed to folllow back: ${error}`))
