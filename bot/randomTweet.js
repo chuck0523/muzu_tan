e57ad68b0ac76ae4b5d8e004e299de35cc185543
chuck <chuckinthestar@gmail.com>
2017-04-18 16:15:19 +0900

    refactor

--- bot/randomTweet.js
+++ bot/randomTweet.js
@@ -6,11 +6,10 @@ module.exports = (Word, twitter, cron) => {
   const tweet = () => {
     console.log('Start to tweet a random word.')
     Word.findRandom()
-    .then(word => {
-      twitter.tweet(`ランダム英単語【${word.name}】: ${word.meaning}`)
-        .then(tweet => console.log(`${tweet} was successfully tweeted.`))
-        .catch(error => console.error(`Failed to tweet ${tweet}.`))
-    })
+      .then(word => `ランダム英単語【${word.name}】: ${word.meaning}`)
+      .then(twitter.tweet)
+      .then(tweet => console.log(`${tweet} was successfully tweeted.`))
+      .catch(error => console.error(`Failed to tweet: ${error}.`))
   }
 
   // set cron task
