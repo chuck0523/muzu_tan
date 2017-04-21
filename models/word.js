bf750d8cf4321e32590d43bd2dff21912c0f852c
chuck <chuckinthestar@gmail.com>
2017-04-21 16:58:01 +0900

    refactor

--- models/word.js
+++ models/word.js
@@ -14,10 +14,8 @@ const Word = mongoose.model('Word', WordSchema)
 // add custom methods
 Word.findRandom = () => {
   return Word.count().exec()
-    .then(count => {
-      const random = Math.floor(Math.random() * count)
-      return Word.findOne().skip(random)
-    })
+    .then(count => Math.floor(Math.random() * count))
+    .then(random => Word.findOne().skip(random))
     .catch(err => console.error('Failed to get random word.'))
 }
 
