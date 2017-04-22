c5967601d00ea5d14b035cd0ea7aca43bafbae78
chuck <chuckinthestar@gmail.com>
2017-04-22 22:46:54 +0900

    add breaks to template string

--- bot/questions.js
+++ bot/questions.js
@@ -4,12 +4,10 @@ module.exports.yontaku = () => {
   return Word.findRandom4()
     .then(words => {
       const answer = words[Math.floor(Math.random() * 4)]
-      return `
-        [問題] ${answer.name}の意味は？
-        1.${words[0].meaning}
-        2.${words[1].meaning}
-        3.${words[2].meaning}
-        4.${words[3].meaning}
-      `
+      return `[問題] ${answer.name}の意味は？\n` +
+        `1.${words[0].meaning}\n` +
+        `2.${words[1].meaning}\n` +
+        `3.${words[2].meaning}\n` +
+        `4.${words[3].meaning}`
     })
 }
