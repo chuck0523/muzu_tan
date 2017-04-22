4332826c5251881d6db7f4ca7139d757207ff136
chuck <chuckinthestar@gmail.com>
2017-04-23 00:54:21 +0900

    change text form

--- bot/question.js
+++ bot/question.js
@@ -4,7 +4,7 @@ module.exports.yontaku = () => {
   return Word.findRandom4()
     .then(words => {
       const answer = words[Math.floor(Math.random() * 4)]
-      return `${answer.name}の意味はどれでしょう？\n` +
+      return `【${answer.name}】の意味はどれでしょう？\n` +
         `(1) ${words[0].meaning}\n` +
         `(2) ${words[1].meaning}\n` +
         `(3) ${words[2].meaning}\n` +
