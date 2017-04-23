45626a9eddc0964e978a366e0a613110078eb192
chuck <chuckinthestar@gmail.com>
2017-04-23 20:02:05 +0900

    move file

---
+++ bot/reply/index.js
@@ -0,0 +1,28 @@
+const twitter = require('../../lib/twitter').getClient()
+const translateApi = require('../../lib/translate')
+
+const { yontaku } = require('../question')
+const { checkAnswer } = require('../answer')
+const { AT_ACCOUNT_NAME, OPTION_NUMBERS } = require('../constants')
+const { isEmptyReply, isAnswerReply } = require('./classifier')
+const { removeAccountName } = require('../formatter')
+
+twitter.selfMentionStream.on('data', (data) => {
+
+  let replyCreator
+  if(isEmptyReply(data.text)) {
+    replyCreator = yontaku()
+  } else if(isAnswerReply(data.text)) {
+    replyCreator = checkAnswer(removeAccountName(data.text), data.in_reply_to_status_id_str)
+  } else {
+    replyCreator = Promise.reject('Input error')
+  }
+
+  replyCreator
+    .then(reply => {
+      console.log(reply)
+      return `@${data.user.screen_name} ${reply}`
+    })
+    .then(tweet => twitter.tweetTo(tweet, data.id_str))
+    .catch(err => console.error('Failed to reply: ', err))
+})
