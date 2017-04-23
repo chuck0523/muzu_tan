a310c2bdb0340a2fece02bbaa9c12fddd5a8954a
chuck <chuckinthestar@gmail.com>
2017-04-23 19:20:39 +0900

    refactor

--- bot/reply.js
+++ bot/reply.js
@@ -1,17 +1,18 @@
 const twitter = require('../lib/twitter').getClient()
-const { Word } = require('../models')
 const translateApi = require('../lib/translate')
 
+const { yontaku } = require('./question')
+const { checkAnswer } = require('./answer')
+
 twitter.selfMentionStream.on('data', (data) => {
-  const text = data.text.replace("@muzu_tan", "").trim()
   const questionIdStr = data.in_reply_to_status_id_str
   const isReply = questionIdStr !== null
 
-  let replyCreator;
-  if(!isReply && text === "") {
-    replyCreator = require('./question').yontaku()
-  } else if(isReply && ['1', '１', '2', '２', '3', '３', '4', '４'].includes(text)) {
-    replyCreator = require('./answer').checkAnswer(text, questionIdStr)
+  let replyCreator
+  if(!isReply && data.text === '@muzu_tan ') {
+    replyCreator = yontaku()
+  } else if(isReply && ['1', '１', '2', '２', '3', '３', '4', '４'].includes(data.text)) {
+    replyCreator = checkAnswer(data.text, questionIdStr)
   } else {
     replyCreator = Promise.reject('Input error')
   }
