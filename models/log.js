6f77748a354cf4eb1eea68e3c2a57c68e6b5e20b
chuck <chuckinthestar@gmail.com>
2017-04-24 21:25:11 +0900

    add field to Log

--- models/log.js
+++ models/log.js
@@ -5,11 +5,16 @@ const { startOfYesterday } = require('date-fns')
 // schema
 const LogSchema = new Schema({
   _id:        { type: Number, required: true },
+
+  // log typs { 1: 'tweet', 2: 'reply', 3: 'follow', 4: 'error', 0: others}
   type:       { type: Number, min: 0, max: 4, required: true },
+
+  // log direction { 0: 'to', 1: 'from' }
+  direction:  { type: Number, min: 0, max: 1 },
+
   log:        { type: String, required: true },
   createdAt:  { type: Date, default: Date.now, index: true },
 })
-// log typs { 1: 'tweet', 2: 'reply', 3: 'follow', 4: 'error', 0: others}
 
 const Log = mongoose.model('Log', LogSchema)
 
