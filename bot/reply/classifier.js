01a68caf6065be3868cfc0b4d85c59faafefbe59
chuck <chuckinthestar@gmail.com>
2017-04-26 02:53:16 +0900

    refactor

--- bot/reply/classifier.js
+++ bot/reply/classifier.js
@@ -1,9 +1,33 @@
 const { AT_ACCOUNT_NAME, OPTION_NUMBERS } = require('../constants')
+const { yontaku } = require('../lib/question')
+const { checkAnswer } = require('../lib/answer')
+const { removeAccountName } = require('../lib/formatter')
+const { ReplyLog } = require('../../models')
 
-module.exports.isEmptyReply = (reply) => {
+const isEmptyReply = (reply) => {
   return reply.trim() === AT_ACCOUNT_NAME
 }
-module.exports.isAnswerReply = (reply) => {
+const isAnswerReply = (reply) => {
   const text = reply.replace(AT_ACCOUNT_NAME, '').trim()
   return OPTION_NUMBERS.includes(text)
+}
+
+module.exports.replyCreator = (reply, tweetId) => {
+  if(isEmptyReply(reply)) {
+    return yontaku()
+  } else if(isAnswerReply(reply)) {
+    return checkAnswer(removeAccountName(reply), tweetId)
+  } else {
+    return Promise.reject('Input error')
+  }
+}
+
+module.exports.loggerCreator = (reply) => {
+  if(isEmptyReply(reply)) {
+    return ReplyLog.saveEmptyReply
+  } else if(isAnswerReply(reply)) {
+    return ReplyLog.saveAnswerReply
+  } else {
+    return ReplyLog.saveOtherReply
+  }
 }
--- bot/reply/index.js
+++ bot/reply/index.js
@@ -1,31 +1,14 @@
 const twitter = require('../../lib/twitter').getClient()
 const translateApi = require('../../lib/translate')
-const { ReplyLog } = require('../../models')
-
-const { yontaku } = require('../lib/question')
-const { checkAnswer } = require('../lib/answer')
-const { isEmptyReply, isAnswerReply } = require('./classifier')
-const { removeAccountName } = require('../lib/formatter')
+const classifier = require('./classifier')
 
 twitter.selfMentionStream.on('data', (data) => {
   const account = `${data.user.name}(@${data.user.screen_name})`
 
-  let replyCreator, logger
-  if(isEmptyReply(data.text)) {
-    replyCreator = yontaku()
-    logger = ReplyLog.saveEmptyReply
-  } else if(isAnswerReply(data.text)) {
-    replyCreator = checkAnswer(removeAccountName(data.text), data.in_reply_to_status_id_str)
-    logger = ReplyLog.saveAnswerReply
-  } else {
-    replyCreator = Promise.reject('Input error')
-    logger = ReplyLog.saveOtherReply
-  }
-
-  replyCreator
+  classifier.replyCreator(data.text, data.in_reply_to_status_id_str)
     .then(reply => `@${data.user.screen_name} ${reply}`)
     .then(tweet => twitter.tweetTo(tweet, data.id_str))
     .catch(err => console.error('Failed to reply: ', err))
 
-  logger(account)
+  classifier.loggerCreator(data.text)(account)
 })
