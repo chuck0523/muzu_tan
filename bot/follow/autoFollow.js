350bd0813ccf5170df09aa96b2a932210699a7d3
chuck <chuckinthestar@gmail.com>
2017-04-26 02:16:57 +0900

    refactor

--- bot/follow/autoFollow.js
+++ bot/follow/autoFollow.js
@@ -2,20 +2,21 @@ const twitter = require('../../lib/twitter').getClient()
 const { FollowLog } = require('../../models')
 
 module.exports = () => {
-  twitter.searchRecentTweets('英語勉強中', 1)
+  twitter.searchRecentTweets('TOEIC900', 1)
     .then(({ statuses }) => {
-      // already following
       if(statuses[0].user.following) {
-        return
+        return Promise.reject('Already following')
       }
       return Promise.all([
         twitter.follow(statuses[0].user.id_str),
         Promise.resolve(statuses[0].text)
       ])
     })
-    .then(results => FollowLog.saveSearchFollow({
-      account: `${results[0].name}(@${results[0].screen_name})`,
-      search_result: results[1],
-    }))
+    .then(results => {
+      FollowLog.saveSearchFollow({
+        account: `${results[0].name}(@${results[0].screen_name})`,
+        search_result: results[1],
+      })
+    })
     .catch(error => console.error('Failed to follow back: ', error))
 }
