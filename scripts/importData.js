da25e7b4c0c4e8e60f5458c4672752c8bc9b0e8b
chuck <chuckinthestar@gmail.com>
2017-04-26 04:37:23 +0900

    refactor

--- scripts/importData.js
+++ scripts/importData.js
@@ -3,7 +3,7 @@ const readFile = Promise.promisify(require('fs').readFile)
 const exec = Promise.promisify(require('child_process').exec)
 
 readFile('database.json', 'utf8')
-  .then(data => JSON.parse(data))
+  .then(JSON.parse)
   .then(data => `mongoimport -h ds161960.mlab.com:61960 -d ${data.MONGO_ACCOUNT_NAME} -c words -u ${data.MONGO_USER_NAME} -p ${data.MONGO_PASSWORD} --file ./data/words.json`)
   .then(exec)
   .catch(console.error)
