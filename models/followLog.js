7cf22182edcfa1006601af9bf37d0d52dab9bed8
chuck <chuckinthestar@gmail.com>
2017-04-26 02:11:18 +0900

    bug fix

--- models/followLog.js
+++ models/followLog.js
@@ -10,7 +10,7 @@ const FollowLogSchema = new Schema({
   follow_type:    { type: Number, min: 0, max: 2 },
   account:        { type: String, required: true },
   search_result:  { type: String },
-  created_at:      { type: Date, default: Date.now, index: true },
+  created_at:     { type: Date, default: Date.now, index: true },
 })
 
 const FollowLog = mongoose.model('FollowLog', FollowLogSchema)
@@ -22,17 +22,17 @@ FollowLog.getYesterdayLogs = () => {
   return FollowLog.find({ created_at: { $gte: startOfYesterday() }}).catch(logError)
 }
 
-FollowLog.saveSearchFollow = (account, search_result) => {
-  new Log({ direction: 0, follow_type: 0, account, search_result }).save().catch(logError)
+FollowLog.saveSearchFollow = ({ account, search_result }) => {
+  new FollowLog({ direction: 0, follow_type: 0, account, search_result }).save().catch(logError)
 }
-FollowLog.saveFollowBack = (account) => {
-  new Log({ direction: 0, follow_type: 1, account }).save().catch(logError)
+FollowLog.saveFollowBack = ({ account }) => {
+  new FollowLog({ direction: 0, follow_type: 1, account }).save().catch(logError)
 }
-FollowLog.saveUnfollow = (account) => {
-  new Log({ direction: 0, follow_type: 2, account }).save().catch(logError)
+FollowLog.saveUnfollow = ({ account }) => {
+  new FollowLog({ direction: 0, follow_type: 2, account }).save().catch(logError)
 }
-FollowLog.saveFollowed = (account) => {
-  new Log({ direction: 1, account }).save().catch(logError)
+FollowLog.saveFollowed = ({ account }) => {
+  new FollowLog({ direction: 1, account }).save().catch(logError)
 }
 
 module.exports = FollowLog
