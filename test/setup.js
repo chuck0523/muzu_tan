23d8e052a429fb34b96fda567c2a66e2c867126e
chuck <chuckinthestar@gmail.com>
2017-05-08 18:52:12 +0900

    comment out twitter client initialiser when testing

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
 // this script is called before Jest test
 require('dotenv').config()
-require('../lib/twitter').initClient()
 require('../lib/mongoose').createConnection()
 
+// TODO: comment out when starting to test twitter features
+// require('../lib/twitter').initClient()
