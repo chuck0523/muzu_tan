08660c462a1fac752def913e7f826490c7ffcfc2
chuck <chuckinthestar@gmail.com>
2017-07-09 18:56:50 +0900

    add one more account search worc

--- bot/search/index.js
+++ bot/search/index.js
@@ -2,7 +2,7 @@ const twitter = require('../../lib/twitter').getClient()
 const { sample } = require('../../lib/util')
 
 const searchEnglishTopic = () => {
-  return twitter.searchTweets(sample(['TOEIC900', 'TOEIC高得点', '難しい英単語', '英語上級者']), 1)
+  return twitter.searchTweets(sample(['TOEIC900', 'TOEIC高得点', '難しい英単語', '英語上級者', '英語ペラペラ']), 1)
     .catch(err => console.error('Failed to search english topics: ', err))
 }
 
